a4fa12e0d5836a57344842b462bcebacaffabd40
Joao Pedro Correia <jpedrocbarros@gmail.com>
2022-08-13 00:25:14 -0300

    nav bar and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Education from "./pages/Education";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        João Pedro Correia
+      </Link>
+      <ul>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/experience">Experience</CustomLink>
+        <CustomLink to="/education">Education</CustomLink>
+      </ul>
+    </nav>
+  );
+
+  function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    );
+  }
+}
